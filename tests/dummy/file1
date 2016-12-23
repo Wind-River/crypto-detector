bd0455210db57320244380e0eae07043d0e43582
Kamyar Kaviani <Kamyar.Kaviani@windriver.com>
2016-12-22 18:20:29 -0800

    Extensive Unit Tests Tests match boundary condition, ignore-case option of keyword search, multi-method search, checksum computation, archive extraction (zip, tar.gz, tar.bz2, tar.lzma, rpm, jar, tar, war, gz, bz2, xz, lzma), recursive archive extraction, ignore_match_types, stop_after option, source-only option, package with no matches, line-text in a match, file-index begin and end in a match, line-index begin and end in a match, match line number, and match text.
    All tests pass.

---
+++ tests/dummy/file1
@@ -0,0 +1,303 @@
+test
+test
+test
+test lorem
+test zlorem
+test loremz
+test z(lorem
+test z.lorem
+test z-lorem
+test z_lorem
+test z(lorem)z
+test z.lorem.z
+test z-lorem-z
+test z_lorem_z
+test lorem)z
+test lorem.z
+test lorem-z
+test lorem_z
+lorem
+zlorem
+loremz
+z(lorem
+z.lorem
+z-lorem
+z_lorem
+z(lorem)z
+z.lorem.z
+z-lorem-z
+z_lorem_z
+lorem)z
+lorem.z
+lorem-z
+lorem_z
+test lorem test
+test zlorem test
+test loremz test
+test z(lorem test
+test z.lorem test
+test z-lorem test
+test z_lorem test
+test z(lorem)z test
+test z.lorem.z test
+test z-lorem-z test
+test z_lorem_z test
+test lorem)z test
+test lorem.z test
+test lorem-z test
+test lorem_z test
+test LOREM test
+test zLOREM test
+test LOREMz test
+test z(LOREM test
+test z.LOREM test
+test z-LOREM test
+test z_LOREM test
+test z(LOREM)z test
+test z.LOREM.z test
+test z-LOREM-z test
+test z_LOREM_z test
+test LOREM)z test
+test LOREM.z test
+test LOREM-z test
+test LOREM_z test
+test ipsum
+test zipsum
+test ipsumz
+test z(ipsum
+test z.ipsum
+test z-ipsum
+test z_ipsum
+test z(ipsum)z
+test z.ipsum.z
+test z-ipsum-z
+test z_ipsum_z
+test ipsum)z
+test ipsum.z
+test ipsum-z
+test ipsum_z
+ipsum
+zipsum
+ipsumz
+z(ipsum
+z.ipsum
+z-ipsum
+z_ipsum
+z(ipsum)z
+z.ipsum.z
+z-ipsum-z
+z_ipsum_z
+ipsum)z
+ipsum.z
+ipsum-z
+ipsum_z
+test ipsum test
+test zipsum test
+test ipsumz test
+test z(ipsum test
+test z.ipsum test
+test z-ipsum test
+test z_ipsum test
+test z(ipsum)z test
+test z.ipsum.z test
+test z-ipsum-z test
+test z_ipsum_z test
+test ipsum)z test
+test ipsum.z test
+test ipsum-z test
+test ipsum_z test
+test IPSUM test
+test zIPSUM test
+test IPSUMz test
+test z(IPSUM test
+test z.IPSUM test
+test z-IPSUM test
+test z_IPSUM test
+test z(IPSUM)z test
+test z.IPSUM.z test
+test z-IPSUM-z test
+test z_IPSUM_z test
+test IPSUM)z test
+test IPSUM.z test
+test IPSUM-z test
+test IPSUM_z test
+test dolor
+test zdolor
+test dolorz
+test z(dolor
+test z.dolor
+test z-dolor
+test z_dolor
+test z(dolor)z
+test z.dolor.z
+test z-dolor-z
+test z_dolor_z
+test dolor)z
+test dolor.z
+test dolor-z
+test dolor_z
+dolor
+zdolor
+dolorz
+z(dolor
+z.dolor
+z-dolor
+z_dolor
+z(dolor)z
+z.dolor.z
+z-dolor-z
+z_dolor_z
+dolor)z
+dolor.z
+dolor-z
+dolor_z
+test dolor test
+test zdolor test
+test dolorz test
+test z(dolor test
+test z.dolor test
+test z-dolor test
+test z_dolor test
+test z(dolor)z test
+test z.dolor.z test
+test z-dolor-z test
+test z_dolor_z test
+test dolor)z test
+test dolor.z test
+test dolor-z test
+test dolor_z test
+test DOLOR test
+test zDOLOR test
+test DOLORz test
+test z(DOLOR test
+test z.DOLOR test
+test z-DOLOR test
+test z_DOLOR test
+test z(DOLOR)z test
+test z.DOLOR.z test
+test z-DOLOR-z test
+test z_DOLOR_z test
+test DOLOR)z test
+test DOLOR.z test
+test DOLOR-z test
+test DOLOR_z test
+test sit
+test zsit
+test sitz
+test z(sit
+test z.sit
+test z-sit
+test z_sit
+test z(sit)z
+test z.sit.z
+test z-sit-z
+test z_sit_z
+test sit)z
+test sit.z
+test sit-z
+test sit_z
+sit
+zsit
+sitz
+z(sit
+z.sit
+z-sit
+z_sit
+z(sit)z
+z.sit.z
+z-sit-z
+z_sit_z
+sit)z
+sit.z
+sit-z
+sit_z
+test sit test
+test zsit test
+test sitz test
+test z(sit test
+test z.sit test
+test z-sit test
+test z_sit test
+test z(sit)z test
+test z.sit.z test
+test z-sit-z test
+test z_sit_z test
+test sit)z test
+test sit.z test
+test sit-z test
+test sit_z test
+test SIT test
+test zSIT test
+test SITz test
+test z(SIT test
+test z.SIT test
+test z-SIT test
+test z_SIT test
+test z(SIT)z test
+test z.SIT.z test
+test z-SIT-z test
+test z_SIT_z test
+test SIT)z test
+test SIT.z test
+test SIT-z test
+test SIT_z test
+test amet
+test zamet
+test ametz
+test z(amet
+test z.amet
+test z-amet
+test z_amet
+test z(amet)z
+test z.amet.z
+test z-amet-z
+test z_amet_z
+test amet)z
+test amet.z
+test amet-z
+test amet_z
+amet
+zamet
+ametz
+z(amet
+z.amet
+z-amet
+z_amet
+z(amet)z
+z.amet.z
+z-amet-z
+z_amet_z
+amet)z
+amet.z
+amet-z
+amet_z
+test amet test
+test zamet test
+test ametz test
+test z(amet test
+test z.amet test
+test z-amet test
+test z_amet test
+test z(amet)z test
+test z.amet.z test
+test z-amet-z test
+test z_amet_z test
+test amet)z test
+test amet.z test
+test amet-z test
+test amet_z test
+test AMET test
+test zAMET test
+test AMETz test
+test z(AMET test
+test z.AMET test
+test z-AMET test
+test z_AMET test
+test z(AMET)z test
+test z.AMET.z test
+test z-AMET-z test
+test z_AMET_z test
+test AMET)z test
+test AMET.z test
+test AMET-z test
+test AMET_z test
--- tests/test_all.py
+++ tests/test_all.py
@@ -13,6 +13,7 @@ OR CONDITIONS OF ANY KIND, either express or implied.
 """
 
 import os
+import time
 from unittest import TestCase
 from cryptodetector import Options, CryptoDetector, MethodFactory
 
@@ -20,59 +21,279 @@ class TestCryptoId(TestCase):
     """Unit Tests
     """
 
-    PACKAGE_HOST = "https://distro.windriver.com/sources/wrlinux-8/"
+    def method(self, method_id):
+        for mc in MethodFactory.method_classes:
+            if mc.method_id == method_id:
+                return mc
 
-    def default_input(self, package):
+    def scan_package(self, test_packages, extra_options={}, keyword_ignore_case=True):
+
         options = Options()._get_options()
-        options["skip_output"] = True
-        options["verbose"] = False
-        options["packages"] = [package]
-        options["suppress_warnings"] = True
-        return options
+        for option in extra_options:
+            options[option] = extra_options[option]
 
-    def count_field(self, data, field, value):
-        return len([match for match in data if match[field] == value])
-
-    def file_count(self, data):
-        return len(set([match["file_path"] for match in data]))
-
-    def get_result(self, method, package, options={}, name=None, ignore_case=False):
-
-        inputs = self.default_input(package)
-        inputs["methods"] = [method]
-
-        for option in options:
-            inputs[option] = options[option]
-
-        package_name = package.split("/")[-1]
-        if name:
-            package_name = name
-
-        self.methods()[method].options["ignore_case"] = ignore_case
-        result = CryptoDetector(inputs, skip_output=True).scan()
-
-        return result[package_name]
-
-    def methods(self):
-        return {mc.method_id: mc for mc in MethodFactory.method_classes}
-
-    def test_keyword_dummy_case_sensitive(self):
         current_directory = os.path.dirname(os.path.realpath(__file__))
-        package = current_directory + "/test_packages/dummy"
-        result = self.get_result("keyword", package)
-        generic_count = self.count_field(result, "match_type", "generic")
-        des_count = self.count_field(result, "match_type", "algorithm/symmetric/block-cipher/DES")
+        options["packages"] = []
+        for package in test_packages:
+            package_full_path = os.path.join(current_directory, package)
+            options["packages"].append(package_full_path)
 
-        self.assertGreater(generic_count, 0)
-        self.assertGreater(des_count, 0)
+        self.method("keyword").options["kwlist_path"] = os.path.join(current_directory, \
+            "dummy_keyword_list.conf")
 
+        self.method("keyword").options["ignore_case"] = keyword_ignore_case
 
-    def test_keyword_case_ignore_case(self):
+        self.method("api").options["kwlist_path"] = os.path.join(current_directory, \
+            "dummy_api_list.conf")
+
+        return CryptoDetector(options, skip_output=True).scan()
+
+    def count_matches(self, data, package, file, match_type, package_name=None):
         current_directory = os.path.dirname(os.path.realpath(__file__))
-        package = current_directory + "/test_packages/dummy"
-        result = self.get_result("keyword", package, ignore_case=True)
-        des_count = self.count_field(result, "match_type", "algorithm/symmetric/block-cipher/DES")
+        file_full_path = os.path.join(current_directory, package)
+        file_full_path = os.path.join(file_full_path, file)
 
-        self.assertGreater(des_count, 0)
+        if package_name is None:
+            package_name = package
 
+        self.assertTrue(file_full_path in data[package_name]["report"])
 
+        count = 0
+        for match in data[package_name]["report"][file_full_path]["matches"]:
+            if match["match_type"] == match_type:
+                count += 1
+        return count
+
+    def assert_result_not_empty(self, result, package):
+        self.assertTrue(package in result)
+        self.assertTrue("report" in result[package])
+        self.assertTrue(result[package]["report"] != {})
+
+    def archive_test(self, archive_type):
+        result = self.scan_package(["extract_test/test." + archive_type], \
+            {"methods": ["keyword"]})
+        self.assert_result_not_empty(result, "test." + archive_type)
+        self.assertEqual(self.count_matches(result, "extract_test/test." + archive_type \
+            , "test", "keyword_boundary_all", "test." + archive_type), 40)
+
+    def archive_test_tar(self, archive_type):
+        """For some reason, sometimes, FileLister extracts tar archives to .tar and then
+        to its files, and sometimes directly to its files."""
+        result = self.scan_package(["extract_test/test." + archive_type], \
+            {"methods": ["keyword"]})
+        self.assert_result_not_empty(result, "test." + archive_type)
+
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        file_full_path = os.path.join(current_directory, "extract_test/test." + archive_type)
+        tar_file_full_path = os.path.join(file_full_path, "test.tar")
+        file_full_path = os.path.join(file_full_path, "test")
+        tar_file_full_path = os.path.join(tar_file_full_path, "test")
+
+        file_path_exists = file_full_path  in result["test." + archive_type]["report"]
+        tar_path_exists = tar_file_full_path in result["test." + archive_type]["report"]
+
+        self.assertTrue(file_path_exists or tar_path_exists)
+
+        if file_path_exists:
+            self.assertEqual(self.count_matches(result, "extract_test/test." + archive_type \
+             , "test", "keyword_boundary_all", "test." + archive_type), 40)
+        else:
+            self.assertEqual(self.count_matches(result, "extract_test/test." \
+                +  archive_type + "/test.tar", "test", "keyword_boundary_all", "test." +
+                archive_type), 40)
+
+    def get_dummy2_matches(self):
+        result = self.scan_package(["dummy", "dummy2"], {"methods": ["keyword"]})
+        self.assert_result_not_empty(result, "dummy2")
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        file_full_path = os.path.join(current_directory, "dummy2")
+        file_full_path = os.path.join(file_full_path, "test")
+        self.assertTrue(file_full_path in result["dummy2"]["report"])
+        matches = result["dummy2"]["report"][file_full_path]["matches"]
+        self.assertEqual(len(matches), 3)
+        return matches
+
+    def test_match_boundary(self):
+        result = self.scan_package(["dummy"], {"methods": ["keyword"]})
+        self.assert_result_not_empty(result, "dummy")
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_any"), \
+            60)
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_boundary_begin"), \
+            48)
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_boundary_end"), \
+            48)
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_boundary_all"), \
+            40)
+
+    def test_keyword_ignore_case(self):
+        result = self.scan_package(["dummy"], {"methods": ["keyword"]}, keyword_ignore_case=False)
+        self.assert_result_not_empty(result, "dummy")
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_any"), \
+            45)
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_boundary_begin"), \
+            36)
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_boundary_end"), \
+            36)
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_boundary_all"), \
+            30)
+
+    def test_multiple_packages(self):
+        result = self.scan_package(["dummy", "dummy2"], {"methods": ["keyword"]})
+        self.assert_result_not_empty(result, "dummy")
+        self.assert_result_not_empty(result, "dummy2")
+
+    def test_checksum(self):
+        result = self.scan_package(["dummy"], {"methods": ["keyword"]})
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        file_full_path = os.path.join(current_directory, "dummy")
+        file_full_path = os.path.join(file_full_path, "file1")
+
+        self.assertEqual(result["dummy"]["report"][file_full_path]["SHA1_checksum"], \
+            "370aef2687f5d68f3696b0190d459600a22dccf7")
+
+    def test_extract_zip(self):
+        self.archive_test("zip")
+
+    def test_extract_tar_bz2(self):
+        self.archive_test_tar("tar.bz2")
+
+    def test_extract_tar_gz(self):
+        self.archive_test_tar("tar.gz")
+
+    def test_extract_tar_lzma(self):
+        self.archive_test_tar("tar.lzma")
+
+    def test_extract_rpm(self):
+        self.archive_test("rpm")
+
+    def test_extract_jar(self):
+        self.archive_test("jar")
+
+    def test_extract_tar(self):
+        self.archive_test("tar")
+
+    def test_extract_war(self):
+        self.archive_test("war")
+
+    def test_extract_gz(self):
+        self.archive_test("gz")
+
+    def test_extract_bz2(self):
+        self.archive_test("bz2")
+
+    def test_extract_xz(self):
+        self.archive_test("xz")
+
+    def test_extract_lzma(self):
+        self.archive_test("lzma")
+
+    def test_extract_recursive_archives(self):
+        result = self.scan_package(["extract_test/recursive.zip"], \
+            {"methods": ["keyword"]})
+        self.assert_result_not_empty(result, "recursive.zip")
+        self.assertEqual(self.count_matches(result, "extract_test/recursive.zip/test.gz", \
+            "test", "keyword_boundary_all", "recursive.zip"), 40)
+        self.assertEqual(self.count_matches(result, "extract_test/recursive.zip/test.bz2", \
+            "test", "keyword_boundary_all", "recursive.zip"), 40)
+        self.assertEqual(self.count_matches(result, "extract_test/recursive.zip/test.zip", \
+            "test", "keyword_boundary_all", "recursive.zip"), 40)
+
+    def test_ignore_match_types(self):
+        result = self.scan_package(["dummy"], {"methods": ["keyword"], \
+            "ignore_match_types": ["keyword_any", "keyword_boundary_end"]})
+        self.assert_result_not_empty(result, "dummy")
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_any"), \
+            0)
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_boundary_begin"), \
+            48)
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_boundary_end"), \
+            0)
+        self.assertEqual(self.count_matches(result, "dummy", "file1", "keyword_boundary_all"), \
+            40)
+
+    def test_stop_after(self):
+        result = self.scan_package(["dummy"], {"methods": ["keyword"], "stop_after": 2})
+        self.assert_result_not_empty(result, "dummy")
+        self.assertEqual(len(result["dummy"]["report"]), 2)
+
+        result = self.scan_package(["dummy"], {"methods": ["keyword"], "stop_after": 1})
+        self.assert_result_not_empty(result, "dummy")
+        self.assertEqual(len(result["dummy"]["report"]), 1)
+
+    def test_source_files_only(self):
+        result = self.scan_package(["dummy"], {"methods": ["keyword"], "source_files_only": True})
+        self.assert_result_not_empty(result, "dummy")
+        self.assertEqual(len(result["dummy"]["report"]), 1)
+
+        current_directory = os.path.dirname(os.path.realpath(__file__))
+        file_full_path = os.path.join(current_directory, "dummy")
+        file_full_path = os.path.join(file_full_path, "file.cpp")
+
+        self.assertTrue(file_full_path in result["dummy"]["report"])
+
+    def test_no_matches(self):
+        result = self.scan_package(["dummy3"], {"methods": ["keyword"]})
+        self.assertTrue("dummy3" in result)
+        self.assertTrue("report" in result["dummy3"])
+        self.assertTrue(result["dummy3"]["report"] == {})
+
+    def test_line_text(self):
+        for match in self.get_dummy2_matches():
+            if match["match_type"] == "keyword_any":
+                self.assertEqual(match["line_text"], "testtestloremtesttest")
+
+            elif match["match_type"] == "keyword_boundary_begin":
+                self.assertEqual(match["line_text"], "testtest IPSUM test")
+
+            elif match["match_type"] == "keyword_boundary_end":
+                self.assertEqual(match["line_text"], "test dolor")
+
+    def test_match_file_index(self):
+        for match in self.get_dummy2_matches():
+            if match["match_type"] == "keyword_any":
+                self.assertEqual(match["match_file_index_begin"], 8)
+                self.assertEqual(match["match_file_index_end"], 13)
+
+            elif match["match_type"] == "keyword_boundary_begin":
+                self.assertEqual(match["match_file_index_begin"], 41)
+                self.assertEqual(match["match_file_index_end"], 46)
+
+            elif match["match_type"] == "keyword_boundary_end":
+                self.assertEqual(match["match_file_index_begin"], 67)
+                self.assertEqual(match["match_file_index_end"], 72)
+
+    def test_match_line_index(self):
+        for match in self.get_dummy2_matches():
+            if match["match_type"] == "keyword_any":
+                self.assertEqual(match["match_line_index_begin"], 8)
+                self.assertEqual(match["match_line_index_end"], 13)
+
+            elif match["match_type"] == "keyword_boundary_begin":
+                self.assertEqual(match["match_line_index_begin"], 9)
+                self.assertEqual(match["match_line_index_end"], 14)
+
+            elif match["match_type"] == "keyword_boundary_end":
+                self.assertEqual(match["match_line_index_begin"], 5)
+                self.assertEqual(match["match_line_index_end"], 10)
+
+    def test_line_number(self):
+        for match in self.get_dummy2_matches():
+            if match["match_type"] == "keyword_any":
+                self.assertEqual(match["match_line_number"], 1)
+
+            elif match["match_type"] == "keyword_boundary_begin":
+                self.assertEqual(match["match_line_number"], 4)
+
+            elif match["match_type"] == "keyword_boundary_end":
+                self.assertEqual(match["match_line_number"], 7)
+
+    def test_match_text(self):
+        for match in self.get_dummy2_matches():
+            if match["match_type"] == "keyword_any":
+                self.assertEqual(match["match_text"], "lorem")
+
+            elif match["match_type"] == "keyword_boundary_begin":
+                self.assertEqual(match["match_text"], "IPSUM")
+
+            elif match["match_type"] == "keyword_boundary_end":
+                self.assertEqual(match["match_text"], "dolor")
