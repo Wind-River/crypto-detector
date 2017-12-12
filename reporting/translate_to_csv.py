871b5c32a86a3c4874a09adb332dd7b68f88b3cd
Kamyar kaviani <kamyar.kaviani87@gmail.com>
2017-12-12 15:15:43 -0800

    Update CSV conversion script

--- reporting/translate_to_csv.py
+++ reporting/translate_to_csv.py
@@ -16,129 +16,110 @@ OR CONDITIONS OF ANY KIND, either express or implied.
 
 Utility to translate .crypto files to CSV format
 """
-
 import argparse
 import json
 import os
 import csv
 
-def read_crypto_file(path):
-    """
-    Read a crypto file and return its content
-
-    Args:
-        path: (string) path to the crypto file
-
-    Returns:
-        (dict) parsed JSON data as a dictionary
-    """
-    try:
-        with open(path, 'r') as file_object:
-            content = file_object.read()
-            data = json.loads(content)
-
-        return data
-
-    except (OSError, IOError) as expn:
-        print("Critical error while reading file " + path + "\n" + str(expn))
-
-def write_data_to_csv(data, path):
-    """
-    Write the crypto data to a csv file
-
-    Args:
-        data: (dict)
-        path: (string) where to write the crypto file
-
-    Returns:
-        None
-    """
-    try:
-        with open(path, 'w') as file:
-            writer = csv.writer(file)
-
-            if "report" not in data:
-                print("Invalid crypto file: " + path)
-                return
-
-            writer.writerow(["File Path", "Algorithm", "Method", "Match", "Line Number", \
-                "Source Matched", "Index Begin", "Index End"])
-
-            for relative_path in data["report"]:
-                for match in data["report"][relative_path]["matches"]:
-
-                    line_number = match["match_line_number"]
-                    source = ""
-                    surrounding_lines = {
-                        -3: match["line_text_before_3"],
-                        -2: match["line_text_before_2"],
-                        -1: match["line_text_before_1"],
-                        0: match["line_text"],
-                        1: match["line_text_after_1"],
-                        2: match["line_text_after_2"],
-                        3: match["line_text_after_3"]
-                        }
-
-                    for i in range(-3, 4):
-                        if line_number + i < 0:
-                            continue
-
-                        if i == 0:
-                            line_number_text = "[" + str(line_number + i) + "]: "
-                        else:
-                            line_number_text = str(line_number + i) + ": "
-
-                        source += line_number_text + surrounding_lines[i] + "\n"
-
-                    writer.writerow([relative_path, match["match_type"], match["method"], \
-                        match["match_text"], match["match_line_number"], source, \
-                        match["match_line_index_begin"], match["match_line_index_end"]])
-
-    except (OSError, IOError) as expn:
-        print("Critical error while creating CSV file " + path + "\n" + str(expn))
+class CryptoReadError(Exception): pass
 
 def process_files():
-    """
-    Read all inputs and process crypto files
+    try:
+        parser = argparse.ArgumentParser(
+            description="Utility to translate .crypto reports to CSV format")
 
-    Args:
-        None
+        parser.add_argument("-o", "--output", \
+           dest="output", default=os.getcwd(), \
+           help="The output directory")
 
-    Returns:
-        None
-    """
-    parser = argparse.ArgumentParser(
-        description="Utility to translate .crypto reports to CSV format")
+        parser.add_argument("crypto_files", nargs='+')
 
-    parser.add_argument("-o", "--output", \
-       dest="output", default=os.getcwd(), \
-       help="The output directory")
+        parsed_args = vars(parser.parse_args())
+        output_directory = parsed_args["output"]
 
-    parser.add_argument("crypto_files", nargs='+')
+        for crypto_file_path in parsed_args["crypto_files"]:
+            if os.path.islink(crypto_file_path):
+                print("Warning: " + crypto_file_path + " is a symbolic link.")
+                continue
 
-    parsed_args = vars(parser.parse_args())
+            extension = crypto_file_path.split(".")[-1]
+            if extension != "crypto":
+                continue
 
-    for path in parsed_args["crypto_files"]:
-        if os.path.islink(path):
-            print("Warning: " + path + " is a symbolic link.")
-            continue
+            filename = os.path.basename(crypto_file_path)
 
-        extension = path.split(".")[-1]
-        if extension != "crypto":
-            continue
+            print("Processing " + filename + " ...")
 
-        filename = path.split("/")[-1]
+            try:
+                with open(crypto_file_path, 'r') as crypto_file:
+                    crypto_data = json.loads(crypto_file.read())
+            except:
+                raise CryptoReadError()
 
-        print("Processing " + filename + " ...")
+            if "crypto_evidence" not in crypto_data:
+                print("Invalid crypto file: " + crypto_file_path \
+                    + ". Missing 'crypto_evidence' field ")
+                continue
 
-        data = read_crypto_file(path)
+            csv_filename = os.path.join(output_directory, filename + ".csv")
 
-        if not data:
-            continue
+            try:
+                with open(csv_filename, 'w') as file:
+                    writer = csv.writer(file)
 
-        write_data_to_csv(data, os.path.abspath(os.path.join(parsed_args["output"], filename \
-            + ".csv")))
+                    writer.writerow(["File Paths", "Evidence Type", "Detection Method", "Match", \
+                        "Line Number", "Source Matched", "Index Begin", "Index End"])
+
+                    for file_sha1 in crypto_data["crypto_evidence"]:
+                        for hit in crypto_data["crypto_evidence"][file_sha1]["hits"]:
+
+                            line_number = hit["line_number"]
+                            source = ""
+                            surrounding_lines = {
+                                -3: hit["line_text_before_3"],
+                                -2: hit["line_text_before_2"],
+                                -1: hit["line_text_before_1"],
+                                0: hit["line_text"],
+                                1: hit["line_text_after_1"],
+                                2: hit["line_text_after_2"],
+                                3: hit["line_text_after_3"]
+                                }
+
+                            for i in range(-3, 4):
+                                if line_number + i < 0:
+                                    continue
+
+                                if i == 0:
+                                    line_number_text = "[" + str(line_number + i) + "]: "
+                                else:
+                                    line_number_text = str(line_number + i) + ": "
+
+                                source += line_number_text + surrounding_lines[i] + "\n"
+
+                            file_paths = "\n".join(crypto_data["crypto_evidence"][file_sha1]["file_paths"])
+
+                            writer.writerow([
+                                file_paths,
+                                hit["evidence_type"],
+                                hit["detection_method"],
+                                hit["matched_text"],
+                                hit["line_number"],
+                                source,
+                                hit["line_index_begin"],
+                                hit["line_index_end"]])
+            except:
+                if os.path.isfile(csv_filename):
+                    os.remove(csv_filename)
+                raise
+
+            print("done")
+
+    except CryptoReadError:
+        print("Failed to open the crypto file and parse its JSON content.")
+
+    except Exception as error:
+        print("Encountered an error while processing the CSV file.\n\n")
+        raise
 
 if __name__ == '__main__':
     process_files()
-    print("done")
